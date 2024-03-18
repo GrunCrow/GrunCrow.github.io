89968462a3e1740ec3e3e56b3c16387aa24a2dda
GrunCrow <57197254+GrunCrow@users.noreply.github.com>
2024-03-18 20:13:20 +0100

    Update webpage

---
+++ links.js
@@ -0,0 +1,5 @@
+// links.js
+const email = "ai.gruncrow@gmail.com";
+const linkedin = "https://www.linkedin.com/in/alba-quez-guez/";
+const twitter = "https://twitter.com/GrunCrow";
+const github = "https://github.com/GrunCrow";
