8d09f90c2af8044ede12d1d7d59b723d61b8ec84
GrunCrow <57197254+GrunCrow@users.noreply.github.com>
2024-03-19 20:55:08 +0100

    Update idk what

--- links.js
+++ links.js
@@ -3,3 +3,4 @@ const email = "ai.gruncrow@gmail.com";
 const linkedin = "https://www.linkedin.com/in/alba-quez-guez/";
 const twitter = "https://twitter.com/GrunCrow";
 const github = "https://github.com/GrunCrow";
+const logo = "images/logos/gruncrow-ai.png"
---
+++ scripts.js
@@ -0,0 +1,5 @@
+// Set email and social media links
+document.getElementById("email").textContent = email;
+document.getElementById("linkedin").href = linkedin;
+document.getElementById("twitter").href = twitter;
+document.getElementById("github").href = github;
